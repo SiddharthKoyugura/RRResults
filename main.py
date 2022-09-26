112634c705522d81900c33a0e52f63390496d002
unknown <koyugurasiddhardha@gmail.com>
2022-09-26 20:30:31 +0530

    responsive debug

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def results():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=3000, debug=True)
